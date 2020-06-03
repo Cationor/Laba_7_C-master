04c9fe1e9cf8fcbcde3ee5b20cfa1a2162d29532
kiryl_kashuba <kashuba.kirya@gmail.ru>
2020-06-03 17:47:54 +0300

    Initial commit

---
+++ Laba_7_C-master/src/Laba_7/Kashuba_9gr/User.java
@@ -0,0 +1,25 @@
+package Laba_7.Kashuba_9gr;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class User {
+    private static String name;
+    private static int addres;
+
+
+    public User (String name, int addres) {
+        this.name = name;
+        this.addres = addres;
+    }
+
+
+
+    public  static String getName() {
+        return name;
+    }
+
+    public static int getAddres() {
+        return addres;
+    }
+}
